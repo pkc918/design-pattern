a3a280d8cd190c598b8f565af65bd6a46d60d7ac
pkc918 <xiaopan2000918@gmail.com>
2024-02-27 07:25:21 +0800

    feat: added unit test for Adapter

---
+++ adapter/go/adapter_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "testing"
+
+func TestAdapterPattern(t *testing.T) {
+	audioPlayer := AudioPlayer{}
+	audioPlayer.Play("mp3", "beyond the horizon.mp3")
+	audioPlayer.Play("mp4", "alone.mp4")
+	audioPlayer.Play("vlc", "far far away.vlc")
+	audioPlayer.Play("avi", "mind me.avi")
+}
+
+/*
+Playing mp3 file. Name: beyond the horizon.mp3
+Playing mp4 file. Name: alone.mp4
+Playing vlc file. Name: far far away.vlc
+Invalid media. avi format not supported
+*/
